1c8059b29e68b1cdd72829fdf4a95d64516422b2
Ian Jones <ijones16@my.whitworth.edu>
2015-04-12 23:29:05 -0700

    Commiting to reverse bad changes

--- OS2015/OS2015/PCB.js
+++ OS2015/OS2015/PCB.js
@@ -1,13 +1,12 @@
 /// <reference path="cpu.ts" />
 var PCB = (function () {
-    function PCB(PID, arrivalTime, burstTime, priority, ioTime, state, timeQuantum) {
+    function PCB(PID, arrivalTime, burstTime, priority, state, timeQuantum) {
         this.PID = PID;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.localTime = burstTime;
         this.priority = priority;
         this.availableState = state;
-        this.ioTime = ioTime;
         this.timeQuantum = timeQuantum;
         this.contextSwitch = 2;
         this.roundRobin = false;
--- OS2015/OS2015/PCB.ts
+++ OS2015/OS2015/PCB.ts
@@ -5,7 +5,6 @@ class PCB {
     burstTime: number;
     localTime: number;
     timeQuantum: number;
-    ioTime: number;
     priority: number;
     availableState: boolean; // if true then available
     arrivalTime: number;
@@ -14,14 +13,13 @@ class PCB {
     contextSwitch: number;
     roundRobin: boolean;
 
-    constructor(PID: number, arrivalTime: number, burstTime: number, priority: number, ioTime: number, state: boolean, timeQuantum: number ) {
+    constructor(PID: number, arrivalTime: number, burstTime: number, priority: number, state: boolean, timeQuantum: number ) {
         this.PID = PID;
         this.arrivalTime = arrivalTime;
         this.burstTime = burstTime;
         this.localTime = burstTime;
         this.priority = priority;
         this.availableState = state;
-        this.ioTime = ioTime;
         this.timeQuantum = timeQuantum;
         this.contextSwitch = 2;
         this.roundRobin = false;
